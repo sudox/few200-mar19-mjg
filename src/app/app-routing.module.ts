3db2545f2ebdfa0210607c6916af192cd01cabf1
Michael <mjg72@zips.uakron.edu>
2019-03-18 15:49:17 -0400

    end of monday

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CommunicationsComponent } from './features/communications/communications.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'communications',
+    component: CommunicationsComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/features/communications/components/entry/entry.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-entry',
+  templateUrl: './entry.component.html',
+  styleUrls: ['./entry.component.css']
+})
+export class EntryComponent implements OnInit {
+
+  @Output() itemAdded = new EventEmitter<string>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  add(item: HTMLInputElement) {
+    this.itemAdded.emit(item.value);
+    item.value = '';
+    item.focus();
+  }
+}
