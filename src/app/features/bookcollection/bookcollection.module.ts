19e5acea622897995be55ac09ea88c4a74dd1dce
Michael <mjg72@zips.uakron.edu>
2019-04-01 16:10:52 -0400

    Added validation, see TODO

--- src/app/features/bookcollection/bookcollection.module.ts
+++ src/app/features/bookcollection/bookcollection.module.ts
@@ -5,12 +5,14 @@ import { EntryComponent } from './components/entry/entry.component';
 import { ListComponent } from './components/list/list.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './reducers';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BookcollectionComponent, EntryComponent, ListComponent],
   imports: [
     CommonModule,
-    StoreModule.forFeature('bookCollectionFeature', reducers)
+    StoreModule.forFeature('bookCollectionFeature', reducers),
+    FormsModule
   ],
   exports: [
     BookcollectionComponent
